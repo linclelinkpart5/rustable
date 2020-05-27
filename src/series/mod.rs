5241a70641f7143353916aa4af3f31b78f4b276f
Mark LeMoine <linclelinkpart5@gmail.com>
2020-05-27 01:56:20 -0700

    Building up `Series`, need to work more on `Index`

--- src/series/mod.rs
+++ src/series/mod.rs
@@ -1,29 +1,71 @@
 
+use std::borrow::Borrow;
+use std::hash::Hash;
+
 use crate::traits::Storable;
 use crate::types::DType;
+use crate::index::Index;
 
 #[derive(Debug, Default)]
-pub struct Series<T>
+pub struct Series<K, V>
 where
-    T: Storable,
+    K: Storable + Hash + Eq,
+    V: Storable,
 {
-    /// Name of the series, used when adding to or extracting from a `Table`.
-    pub name: Option<String>,
-
-    /// Raw storage for the contained values.
-    pub values: Vec<T>,
+    index: Option<Index<K>>,
+    values: Vec<V>,
 }
 
-impl<T> Series<T>
+impl<K, V> Series<K, V>
 where
-    T: Storable,
+    K: Storable + Hash + Eq,
+    V: Storable,
 {
-    // fn dtype_associated() -> DType {
-    //     T::dtype()
-    // }
-
     /// Returns the `DType` of this `Series`.
     pub fn dtype(&self) -> DType {
-        T::dtype()
+        V::dtype()
+    }
+
+    /// Returns a readonly reference the the `Index` contained in this `Series`,
+    /// if there is one.
+    pub fn index(&self) -> Option<&Index<K>> {
+        self.index.as_ref()
+    }
+
+    /// Consumes the `Series` and returns the value data store.
+    pub fn into_raw(self) -> Vec<V> {
+        self.values
+    }
+
+    /// Returns a readonly slice of the value data store of this `Series`.
+    pub fn as_slice(&self) -> &[V] {
+        &self.values
+    }
+
+    /// Returns a mutable slice of the value data store of this `Series`.
+    pub fn as_mut_slice(&mut self) -> &mut [V] {
+        &mut self.values
+    }
+
+    /// Given a key, returns a readonly reference to its value in the `Series`,
+    /// if it exists.
+    pub fn loc<Q>(&self, _loc: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        // Always return `None` if there is no index.
+        todo!("Need to implement lookup on `Index` first");
+    }
+
+    /// Given a key, returns a mutable reference to its value in the `Series`,
+    /// if it exists.
+    pub fn loc_mut<Q>(&mut self, _loc: &Q) -> Option<&mut V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        // Always return `None` if there is no index.
+        todo!("Need to implement lookup on `Index` first");
     }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -8,12 +8,3 @@ pub trait Storable: Debug + Clone + Send + Sized {
     /// Returns the [`DType`] of this type.
     fn dtype() -> DType;
 }
-
-/// Core interface used for indices.
-pub trait Indexer<L>
-where
-    L: Storable,
-{
-    /// Translates a loc-style key into a raw iloc-style key.
-    fn loc_to_iloc(&self, loc: &L) -> Option<usize>;
-}
